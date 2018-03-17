4d5fee5e150e40ac647a8e68458650889c90c5ce
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-17 23:29:26 +0000

    Basic configuration of the project.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+require 'features/web_helpers'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = BookmarkManager
+
+SCF = SimpleCov::Formatter
+formatters = [SCF::Console, SCF::HTMLFormatter]
+SimpleCov.formatter = SCF::MultiFormatter.new(formatters)
+
+SimpleCov.start
