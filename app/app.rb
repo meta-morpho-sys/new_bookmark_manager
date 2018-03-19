fc17f59d6e47c514297ffdf7d68ab8a9f0d2442f
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-19 12:50:58 +0000

    Testing out the environments. Tests green.

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,16 @@
+require 'sinatra/base'
 # Controller
-class BookmarkManager
+class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    links = [
+      'https://online.lloydsbank.co.uk/personal/logon/login.jsp?messageKey=IB:9210358&mobile=false',
+      'https://www.borrowmydoggy.com/search/dogs',
+      'http://vogliadicucina.blogspot.co.uk/2015/11/biscotti-con-avena-e-albicocche.html'
+    ]
+    links.join
+  end
+
+
+  run! if app_file == $PROGRAM_NAME
 end
