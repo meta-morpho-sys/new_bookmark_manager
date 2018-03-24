1ae39443d37b41b32b5bf8341abddf57eeac7874
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-24 20:48:30 +0000

    Feature for verifying if the DB connection is persistent.

--- models/database_connection.rb
+++ models/database_connection.rb
@@ -5,6 +5,10 @@
 class DatabaseConnection
 
   def self.setup(db_name)
-    @connection = PG.connect(db_name: db_name)
+    @connection = PG.connect(dbname: db_name)
+  end
+
+  def self.connection
+    @connection
   end
 end
--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -5,10 +5,14 @@ require './models/database_connection'
 describe DatabaseConnection do
   describe'.setup' do
     it 'sets up a connection to the database through PG' do
-      expect(PG).to receive(:connect).with(db_name: 'my_database')
+      expect(PG).to receive(:connect).with(dbname: 'my_database')
       DatabaseConnection.setup('my_database')
     end
 
+    example 'the connection is persistent' do
+      conn = DatabaseConnection.setup('new_bookmark_manager_test')
+      expect(DatabaseConnection.connection).to eq conn
+    end
   end
 
 end
