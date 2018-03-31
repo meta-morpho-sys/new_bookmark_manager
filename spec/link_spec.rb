a9a7b00272f7e9485c157d0d845773a15396b288
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-01 00:02:45 +0100

    Dried out the tests.

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -4,28 +4,25 @@ require_relative '../models/link'
 
 describe Link do
   let(:links) { Link.all }
+  let(:urls) { links.map(&:url) }
+  let(:titles) { links.map(&:title) }
   describe '.all' do
     it 'returns all links wrapped in links instances' do
-      urls = links.map(&:url)
-      expect(urls).to include('https://online.lloydsbank.co.uk')
-      expect(urls).to include('https://www.borrowmydoggy.com/')
-      expect(urls).to include('http://vogliadicucina.blogspot.co.uk')
+      expect(titles).to include 'Lloyds'
+      expect(titles).to include 'Doggy'
+      expect(titles).to include 'Recipes'
     end
   end
 
   describe '.create' do
     it 'adds a new link' do
       Link.create 'http://www.new-test-link.com', 'Test title'
-      urls = links.map(&:url)
-      titles = links.map(&:title)
       expect(titles).to include 'Test title'
       expect(urls).to include('http://www.new-test-link.com')
     end
 
     it 'does not add a new link if it is not valid' do
       Link.create 'not a real link', 'Not real title hehehe'
-      urls = links.map(&:url)
-      titles = links.map(&:title)
       expect(titles).not_to include 'Not real title hehehe'
       expect(urls).not_to include 'not a real link'
     end
@@ -33,7 +30,8 @@ describe Link do
 
   describe '.delete' do
     it 'deletes a link' do
-      Link.delete '1'
+      Link.delete '2'
+      expect(titles).not_to include 'Lloyds'
       expect(links).not_to include 'https://online.lloydsbank.co.uk'
     end
   end
