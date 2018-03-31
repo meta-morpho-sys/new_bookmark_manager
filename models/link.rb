e7a700085f6818b0f37ba78356a8a873c0bde93c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-03-31 01:08:53 +0100

    Cleanup

--- models/link.rb
+++ models/link.rb
@@ -5,11 +5,15 @@ require './lib/db_connector'
 # Connects to the DB via DatabaseConnection
 # Makes queries to create and fetch the links from the DB.
 class Link
+  # TODO :Use a 'max_length' CONSTANT to reference link length in the app
+  # LINK_MAX_LENGTH = 1000
+
   def self.all
     result = DbConnector.query 'SELECT * FROM links'
     result.map { |link| link['url'] }
   end
 
+  # TODO : Raise Exception if user tries to save the same link more than once.
   def self.create(url)
     return false unless a_url?(url)
     DbConnector.query("INSERT INTO links (url) VALUES('#{url}')")
