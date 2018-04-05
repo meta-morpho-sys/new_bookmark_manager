3d5dc70145e78bc1b05d3c0d817cee3dee3417f7
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-05 11:31:22 +0100

    Cleanup.

--- spec/features/update_a_link_spec.rb
+++ spec/features/update_a_link_spec.rb
@@ -3,18 +3,17 @@
 feature 'Updating a link' do
   scenario 'a user can modify the title and the url of an existing link' do
     visit '/'
+
     within '#link-3' do
-      click_button 'Update'
+      click_button 'Edit'
     end
 
     expect(current_path).to eq '/update_link'
     fill_in 'new_url', with: 'https://www.google.co.uk'
     fill_in 'new_title', with: 'Google'
-    click_button 'Confirm the update'
+    click_button 'Confirm the edit'
 
     expect(page).not_to have_content 'Recipes'
-    expect(page).not_to have_link 'http://vogliadicucina.blogspot.co.uk'
     expect(page).to have_content 'Google'
-    expect(page).to have_link 'https://www.google.co.uk'
   end
 end
