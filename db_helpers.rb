3d8afd2fe2a0306513976a2fcf7ebb5861f54938
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-06 20:57:24 +0100

    Tidying the Rakefile.

--- db_helpers.rb
+++ db_helpers.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
+def create_if_needed(db_name)
+  if db_exists?(db_name)
+    puts "NOTICE:  database \"#{db_name}\" already exists, skipping"
+  else
+    connection.exec("CREATE DATABASE #{db_name}")
+  end
+end
 
+def db_exists?(db_name)
+  conn = PG.connect
+  res = conn.exec(
+      "SELECT count(*) FROM pg_database WHERE datname ='#{db_name}'"
+  )
+  res.cmd_tuples == 1
+end
+
+def insert_link(num, url, title)
+  DbConnector.query("INSERT INTO links VALUES(#{num}, '#{url}', '#{title}')")
+end
+
+def insert_comment(num, text, link_id)
+  DbConnector.query("INSERT INTO comments VALUES(#{num},'#{text}',#{link_id})")
+end
