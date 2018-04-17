4788234a4abccfcd991faddf7f13c8f9e871e959
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-17 17:40:24 +0100

    User can be autenticated

--- app.rb
+++ app.rb
@@ -16,9 +16,18 @@ class BookmarkManager < Sinatra::Base
   register Sinatra::Flash
 
   get '/' do
-    erb :'login/login'
+    erb :home
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    session[:user_id] = user.id
+    redirect '/bookmarks'
+  end
 
   # <editor-fold desc="Users">
   get '/users/new' do
