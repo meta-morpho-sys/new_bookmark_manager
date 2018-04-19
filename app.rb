c88489c5eee7f6ce2f33d78936e42492ddbc2fde
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-19 13:43:52 +0100

    Updated

--- app.rb
+++ app.rb
@@ -19,12 +19,14 @@ class BookmarkManager < Sinatra::Base
     erb :home
   end
 
+  # <editor-fold desc="Sessions">
   get '/sessions/new' do
     erb :'sessions/new'
   end
 
   post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
+
     if user
       session[:user_id] = user.id
       redirect "/user/#{user.id}/bookmarks"
@@ -33,16 +35,13 @@ class BookmarkManager < Sinatra::Base
       redirect '/sessions/new'
     end
   end
-  post '/user/:id/sessions/destroy' do
+
+  post '/user /:id/sessions/destroy' do
     session.clear
     flash[:notice] = 'You successfully signed out'
     redirect '/'
   end
-  # post '/sessions/destroy' do
-  #   session.clear
-  #   flash[:notice] = 'You successfully signed out'
-  #   redirect '/'
-  # end
+  # </editor-fold>
 
   # <editor-fold desc="Users">
   get '/users/new' do
@@ -52,33 +51,21 @@ class BookmarkManager < Sinatra::Base
   post '/users' do
     user = User.create(params[:email], params[:password])
     session[:user_id] = user.id
-    redirect '/bookmarks'
+    redirect "/user/#{user.id}/bookmarks"
   end
-
-  # get '/user/:id/bookmarks' do
-  #   'Hello world'
-  # end
   # </editor-fold>
 
   # <editor-fold desc="Bookmarks">
-  get '/bookmarks' do
-    redirect '/user/:id/bookmarks'
-  end
-
   get '/user/:id/bookmarks' do
     @user = User.find(session[:user_id])
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
   end
-  # get '/bookmarks' do
-  #   @user = User.find(session[:user_id])
-  #   @bookmarks = Bookmark.all
-  #   erb :'bookmarks/index'
-  # end
 
   post '/bookmarks/new' do
     begin
-      bookmark = Bookmark.create(params[:url], params[:title])
+      user = User.find(session[:user_id])
+      bookmark = Bookmark.create(params[:url], params[:title], params[:user_id])
       flash[:notice] = 'You must submit a valid URL.' unless bookmark
     rescue PG::UniqueViolation
       flash[:notice] = 'That title is already taken, choose another.'
@@ -86,16 +73,17 @@ class BookmarkManager < Sinatra::Base
       flash[:notice] = 'Something went wrong with the database. \
                         This sometimes happens, please try again.'
     end
-    redirect '/bookmarks'
+    redirect "/user/#{user.id}/bookmarks"
   end
 
   delete '/bookmarks/:id/delete' do
     Bookmark.delete(params[:id])
     flash[:notice] = "Bookmark **#{params[:title]}** was successfully deleted!"
-    redirect '/bookmarks'
+    redirect "/user/#{user.id}/bookmarks"
   end
 
   get '/bookmarks/:id/edit' do
+    @user = User.find(session[:user_id])
     @bookmark = Bookmark.find(params[:id])
     erb :'bookmarks/edit'
   end
@@ -103,6 +91,7 @@ class BookmarkManager < Sinatra::Base
   patch '/bookmarks/:id/update' do
     begin
       bookmark = Bookmark.update(params[:id], params['new_url'], params['new_title'])
+      user = User.find(session[:user_id])
       flash[:notice] = 'You must submit a valid URL' unless bookmark
     rescue PG::UniqueViolation
       flash[:notice] = 'That title is already taken, choose another.'
@@ -110,22 +99,27 @@ class BookmarkManager < Sinatra::Base
       flash[:notice] = 'Something went wrong with the database. \
                         This sometimes happens, please try again.'
     end
-    redirect '/bookmarks'
+    p ' from patch'
+    p user.id
+    redirect "/user/#{user.id}/bookmarks"
   end
   # </editor-fold>
 
   # <editor-fold desc="Comments">
   get '/bookmarks/:id/comments' do
     @bookmark = Bookmark.find(params[:id])
+    @user = User.find(session[:user_id])
     erb :'comments/new'
   end
 
   post '/bookmarks/comments/new' do
+    user = User.find(session[:user_id])
     Comment.create(params[:text], params[:id])
-    redirect '/bookmarks'
+    redirect "/user/#{user.id}/bookmarks"
   end
 
   get '/bookmark/:id/comments/view' do
+    @user = User.find(session[:user_id])
     @bookmark = Bookmark.find(params[:id])
     @comments = @bookmark.comments
     erb :'comments/view'
@@ -134,18 +128,21 @@ class BookmarkManager < Sinatra::Base
 
   # <editor-fold desc="Tags">
   get '/bookmarks/:id/tags' do
+    @user = User.find(session[:user_id])
     @bookmark = Bookmark.find(params[:id])
     erb :'tags/new'
   end
 
   post '/bookmarks/tags/new' do
+    user = User.find(session[:user_id])
     tag = Tag.create(params[:content])
     BookmarkTag.create(params[:id], tag.id)
     flash[:notice] = "**#{params[:content]}** tag successfully created!"
-    redirect '/bookmarks'
+    redirect "/user/#{user.id}/bookmarks"
   end
 
   get '/tags/:id/bookmarks' do
+    @user = User.find(session[:user_id])
     @bookmarks = Tag.find(params[:id]).bookmarks
     erb :'tags/bookmarks/index'
   end
