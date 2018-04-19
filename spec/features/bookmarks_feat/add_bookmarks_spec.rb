d1c37aaa8448a2abd9fd77912ae5d51b2359bceb
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-19 13:44:11 +0100

    'Corrected the require paths to the tested files.'

--- spec/features/bookmarks_feat/add_bookmarks_spec.rb
+++ spec/features/bookmarks_feat/add_bookmarks_spec.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 feature 'Adding bookmarks' do
-  before { visit '/bookmarks' }
+  before(:each) do
+    user = User.create('test@example.com', 'password123')
+    login(user)
+  end
+
   context 'adding real bookmarks' do
     scenario 'the user can add a bookmark to Bookmark Manager' do
       fill_in_and_add
@@ -9,10 +13,12 @@ feature 'Adding bookmarks' do
     end
 
     scenario 'title duplications are not allowed' do
-      fill_in_and_add
-      expect(current_path).to eq '/user/:id/bookmarks'
+      user = User.create('test2@example.com', 'password123')
+      login(user)
 
       fill_in_and_add
+      fill_in_and_add
+
       expect(page).to have_content 'That title is already taken, choose another.'
     end
   end
