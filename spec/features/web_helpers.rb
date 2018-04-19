1a2136374fce3e79bceedd767f054f237fa97c16
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-19 13:34:12 +0100

    Login helper method

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -5,3 +5,13 @@ def fill_in_and_add
   fill_in 'title', with: 'Test title'
   click_button 'Add'
 end
+
+def login(user)
+  visit '/'
+  click_link 'Sign in'
+  expect(current_path).to eq '/sessions/new'
+
+  fill_in('email', with: user.email)
+  fill_in('password', with: 'password123')
+  click_button 'Sign me in'
+end
