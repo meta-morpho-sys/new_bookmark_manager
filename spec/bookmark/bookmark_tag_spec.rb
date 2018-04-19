c91e4d5eb78740ca9e45a75bb7d15046c90a07ad
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-19 13:48:37 +0100

    Update

--- spec/bookmark/bookmark_tag_spec.rb
+++ spec/bookmark/bookmark_tag_spec.rb
@@ -5,7 +5,8 @@ require_relative '../../models/bookmark_tag'
 describe BookmarkTag do
   describe '.create' do
     it 'creates a new join between links and tags' do
-      bm = Bookmark.create('https://www.borrowmydoggy.com/', 'Doggy')
+      user = User.create('test@example', 'password123')
+      bm = Bookmark.create('https://www.borrowmydoggy.com/', 'Doggy', user.id)
       tag = Tag.create('Health and Well-being')
       bm_tag = BookmarkTag.create(bm.id, tag.id)
       expect(bm_tag).not_to be_nil
