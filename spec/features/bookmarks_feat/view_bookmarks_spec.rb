84f69f50cba9c91c3a9a03f2cbfdb77c9c97f7ba
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-19 14:35:53 +0100

    Update

--- spec/features/bookmarks_feat/view_bookmarks_spec.rb
+++ spec/features/bookmarks_feat/view_bookmarks_spec.rb
@@ -2,8 +2,11 @@
 
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
-    visit '/bookmarks'
-    Bookmark.create('https://online.lloydsbank.co.uk', 'Lloyds')
+    user = User.create('test@example', 'password123')
+
+    login user
+
+    Bookmark.create('https://online.lloydsbank.co.uk', 'Lloyds', user.id)
     click_button 'Add'
     expect(page).to have_content 'Lloyds'
   end
