88d00a6bd62b31df11ea07237fcb4946ed05174e
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-21 18:44:30 +0100

    Tidying up

--- spec/features/tags_feat/add_tags_spec.rb
+++ spec/features/tags_feat/add_tags_spec.rb
@@ -16,29 +16,10 @@ feature 'Creating a tag for a bookmark' do
     fill_in(:content, with: 'Fun')
     click_button 'Submit'
 
-    expect(page).to have_content '**Fun** tag successfully created!'
+    expect(page).to have_content "'Fun' tag successfully created"
 
     within "#bookmark-#{bm.id}" do
       expect(page).to have_content 'Fun'
     end
-  end
-
-  scenario 'anyone can see the bookmarks filtered by tag' do
-    user = User.create('test@example', 'password123')
-    bm = Bookmark.create('https://www.netflix.com', 'Neflix', user.id)
-    tag = Tag.create 'Movies and fun'
-    BookmarkTag.create(bm.id, tag.id)
-    login user
-
-    # within "#bookmark-#{tag.id}" do
-    #   click_link 'Movies and fun'
-    # end
-
-    within "#bookmark-#{bm.id}" do
-      click_link 'Movies and fun'
-    end
-
-    expect(current_path).to eq "/tags/#{tag.id}/bookmarks"
-    expect(page).to have_content 'Netflix'
   end
 end
