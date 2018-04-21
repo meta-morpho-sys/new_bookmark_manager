2a913d47edf6df36df083b94945cb36dd781c469
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-04-21 18:46:55 +0100

    Better location for the test for filtered bookmarks.

--- spec/features/bookmarks_feat/view_bookmarks_spec.rb
+++ spec/features/bookmarks_feat/view_bookmarks_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 feature 'Viewing bookmarks' do
-  scenario 'A user can see bookmarks' do
+  scenario 'A user can see all bookmarks' do
     user = User.create('test@example', 'password123')
 
     login user
@@ -14,5 +14,21 @@ feature 'Viewing bookmarks' do
 
     expect(page).to have_content 'Lloyds'
     expect(page).to have_content 'Barkley'
+  end
+
+
+  scenario 'a user can see the bookmarks filtered by tag' do
+    user = User.create('test@example', 'password123')
+    bm = Bookmark.create('https://www.netflix.com', 'Neflix', user.id)
+    tag = Tag.create 'Movies and fun'
+    BookmarkTag.create(bm.id, tag.id)
+    login user
+
+    within "#bookmark-#{bm.id}" do
+      click_link 'Movies and fun'
+    end
+
+    expect(current_path).to eq "/tags/#{tag.id}/bookmarks"
+    expect(page).to have_content 'Netflix'
   end
 end
